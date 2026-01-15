1c582afc444db6578817ffdf829d963c4e93129e
Odrick <odrick.ngouama@ext.leroymerlin.fr>
2026-01-15 11:23:16 +0100

    Refactoring et ajout des tests

--- src/test/java/com/testtechnique/todoservice/bdd/CucumberIT.java
+++ src/test/java/com/testtechnique/todoservice/bdd/CucumberIT.java
@@ -12,11 +12,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-<<<<<<< HEAD
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.testtechnique.todoservice.bdd")
-=======
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.testtechnique.todoservice.bdd.steps")
->>>>>>> 95973b7 (Refactoring et ajout des tests)
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class CucumberIT {
 }
